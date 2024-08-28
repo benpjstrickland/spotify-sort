35e475ff83475c634e9f1dc0e9294c2e11ad823c
benPC <ben.strickland@dal.ca>
2024-08-28 11:34:55 -0300

    cleaned up some code and removed curl

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # Import libraries and files
 from dotenv import load_dotenv
-import os 
-import json
-import base64
+import os
 from requests import post, get
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 load_dotenv() # Load .env for CLIENT_ID and CLIENT_SECRET
 
@@ -11,35 +11,9 @@ load_dotenv() # Load .env for CLIENT_ID and CLIENT_SECRET
 client_id = os.getenv('CLIENT_ID')
 client_secret = os.getenv('CLIENT_SECRET')
 
-<<<<<<< HEAD
-
 # Main Method
 def main():
     print("Welcome to Spotify Sort! To start, let's login to your Spotify account.")
-
+    print(client_id, client_secret) # testing
 
 main()
-# Methods
-=======
-print(client_id, client_secret)
->>>>>>> 134866fdc486064cddf2a8d1e14bc62c9b4afbbd
-
-def get_token(): 
-    auth_string = client_id + ":" + client_secret
-    auth_bytes = auth_string.encode("utf-8") #encode auth string into bytes
-    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8") # encodes string in base 64 & convert back to utf-8
-
-    url = "https://accounts.spotify.com/api/token" 
-    headers = {     # dictionary with headers + encoded authentication string
-        "Authorization": "Basic " + auth_base64, 
-        "Content-Type": "application/x-www-form-urlencoded"
-    }
-
-    data = {"grant_type": "client_credentials"} # data to be sent in POST request
-    result = post(url, headers = headers, data = data) # Send post request to url with headers and data
-    json_result = json.loads(result.content) # parse json response content into python
-    token = json_result["access_token"] # Extract access token from json response
-    return token 
-
-def get_auth_header(token): # Take oAuth token & return dictionary with authorization
-    return {"Authorization": "Bearer " + token}
